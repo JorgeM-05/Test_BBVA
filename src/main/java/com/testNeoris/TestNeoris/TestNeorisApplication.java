3ca79e7871bf3d1fa8507574ad009f4088236111
Jorge Muñoz <51250678+JorgeM-05@users.noreply.github.com>
2022-06-23 16:37:11 -0500

    package

---
+++ src/main/java/com/testNeoris/TestNeoris/TestNeorisApplication.java
@@ -0,0 +1,13 @@
+package com.testNeoris.TestNeoris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNeorisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNeorisApplication.class, args);
+	}
+
+}
